e182449a6347b01a59db9f7955923e2e489f36fa
Niraj Paul <niraj.paul@sainsburys.co.uk>
2020-07-14 19:53:41 +0100

    Improve code

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import React, { useRef, useEffect, useState } from 'react';
 
 import { homeAnimation } from "../animations/animations";
-import Overlay from "../components/Overlay"; 
+import Overlay from "../components/Overlay";
 import Banner from "../components/Banner";
 import Cases from "../components/Cases";
 
@@ -9,8 +9,8 @@ const Home = ({dimensions}) => {
   const [animationComplete, setanimationComplete] = useState(false);
   const [cases, setData] = useState([]);
   const completeAnimation = () => setanimationComplete(true);
-
   const [loaded, setLoaded] = useState(false);
+  
   const counter = useRef(0);
   const imageLoaded = () => {
     counter.current += 1;
@@ -18,17 +18,18 @@ const Home = ({dimensions}) => {
       setLoaded(true);
     }
   }
+  const photoApi = 'https://picsum.photos';
     
   useEffect(() => {
     loaded && homeAnimation(completeAnimation);
 
-    fetch('https://picsum.photos/v2/list?page=2&limit=9')
+    fetch(`${photoApi}/v2/list?page=2&limit=9`)
       .then(response => response.json())
       .then(data => {
         // Resize the image files
-        const updateData = data.map(obj => {
-          return Object.assign(obj, { download_url: `https://picsum.photos/id/${obj.id}/600/400` });
-        });
+        const updateData = data.map(obj => Object.assign(obj, {
+          download_url: `${photoApi}/id/${obj.id}/600/400`
+        }));
         setData(updateData);
       })
       .catch(error => console.log("Sorry something went wrong", error));
